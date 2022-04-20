48b67b2cf81413f00d912b08e58dffd65fa57158
Stergyos Katsogyannis <steryios@hotmail.com>
2022-04-20 09:56:55 +0300

    Overflow of a short.

--- Arrptr.cpp
+++ Arrptr.cpp
@@ -6,20 +6,9 @@ using namespace std;
 
 
 int main(void){
-    vector<int> vec{ 90,2,3,4 };
+    short n = INT16_MAX;
 
-    auto b = * (vec.begin() + 3);
-
-
-    /*int x=9, y=10, z=2;
-
-    int w = x * y + z;*/
-
-    int i = 0;
-    cout << i << " " << i++ << endl;
-
-    int x = (b < 0) ? 1 : 0;
-    cout << x << endl;
+    cout << n << endl;
 
     system("pause>null");
     return 0;
