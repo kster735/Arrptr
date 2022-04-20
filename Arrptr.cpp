f88335d07a6aa45fa56a90c2348e78b30518087f
Stergyos Katsogyannis <steryios@hotmail.com>
2022-04-20 08:06:37 +0300

    a new commit with summary

--- Arrptr.cpp
+++ Arrptr.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 
 int main(void){
-   
+    bool t = true;
+    bool f = false;
+
+    cout << sizeof t << endl;
+
+    /// <summary>
+    /// A new commit after 655ad480
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
 
     system("pause>null");
     return 0;
