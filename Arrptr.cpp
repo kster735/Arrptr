3e92e0100d1bbfbb25ca93053ea0734e3399b0fd
Stergyos Katsogyannis <steryios@hotmail.com>
2022-04-20 08:17:26 +0300

    R and L values demo

--- Arrptr.cpp
+++ Arrptr.cpp
@@ -6,10 +6,18 @@ using namespace std;
 
 
 int main(void){
-    bool t = true;
+    const bool t = true;
     bool f = false;
 
+    f = t;
+
     cout << sizeof t << endl;
+
+    decltype(t) boolean1 = false; //needs initialization because const t yields an lvalue
+    //and lvalues return an lvalue with decltype.
+
+
+    decltype(f) boolean2; //no need for initialization
 
     /// <summary>
     /// A new commit after 655ad480
